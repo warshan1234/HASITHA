8e21c21fbb6c615bcd6cfda9f71a1f8dd6a69741
warshan1234 <navisasu09@gmail.com>
2025-03-08 18:47:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=XiRgQIpL#XlRhyNNM0ErcrN-QF_zmCRpEbYs730Av6FgljprgPZQ' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=6qxlXbDa#vR91o5Dp-Wg1NYlKH0cnq-hktM5rgWxFIy5b9DPUDOM' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
